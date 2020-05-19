40563deb132e6b167cf2ce88fc1fb4404e96ebf7
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2020-05-19 12:38:53 +0300

    Updates

---
+++ precache-manifest.4a468aaafefe97878ab230de52ecd203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3ba44b6d7d3d556cdfad5231b9296e",
+    "url": "/msib/index.html"
+  },
+  {
+    "revision": "0ecae9c99d9d671d0880",
+    "url": "/msib/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "70afff6c089cbf18083d",
+    "url": "/msib/static/js/2.56fa325f.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/msib/static/js/2.56fa325f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecae9c99d9d671d0880",
+    "url": "/msib/static/js/main.903003bb.chunk.js"
+  },
+  {
+    "revision": "a3fb884aa4f9a509397f",
+    "url": "/msib/static/js/runtime-main.ff5495f8.js"
+  }
+]);
